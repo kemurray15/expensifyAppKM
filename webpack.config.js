519abca610d71a6dc7bd00b220c7782e059f323e
Kevin Murray <kmur15@gmail.com>
2018-11-08 15:47:47 -0500

    setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -43,12 +43,13 @@ module.exports = (env) => {
         devtool: isProduction? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true //this tells the dev server that client-side code will handle routing 
+            historyApiFallback: true, //this tells the dev server that client-side code will handle routing 
             //and this page (public?) is what should be served up for all 404 routes - I think basically
             //just pass back index.html and let react-router figure out what to render
             //The dev-server build notes now show the message "404s will fallback to /index.html" AKA 
             //when the server can't find what is in the HTTP Request, the client side code will also
             //be searched to see if there is something available to render
+            publicPath: '/dist/'
         }
     }
 };
